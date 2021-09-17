d0fd07a781b25e46891ec22189f31c3040c50c92
Ben <ohgoldust@yahoo.fr>
2021-09-17 09:08:05 +0200

    feat(controller): add a route to see details of a product
    Closes #7

--- src/Controller/SmartPhonesController.php
+++ src/Controller/SmartPhonesController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\SmartPhone;
+use App\Exceptions\ResourceNotFoundException;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Request\ParamFetcherInterface;
+use OpenApi\Annotations as OA;
+use OpenApi\Annotations\JsonContent;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 class SmartPhonesController extends AbstractFOSRestController
 {
@@ -23,10 +26,18 @@ class SmartPhonesController extends AbstractFOSRestController
      *     default="asc",
      *     description="Price sort order (asc or desc)" 
      * )
-     * 
      * @Rest\View( 
      *     statusCode = 200, 
-     *     serializerGroups = {"list"} 
+     *     serializerGroups = {"list"}
+     * )
+     * 
+     * @OA\Get(
+     *      tags={"Smartphones"},
+     *      @OA\Response(
+     *          response="200",
+     *          description="Our smartphones",
+     *          @OA\JsonContent(type="array", @OA\Items(ref=@Model(type=SmartPhone::class, groups={"list"})))
+     *      )
      * )
      */
     public function list($brand, $price)
@@ -35,7 +46,46 @@ class SmartPhonesController extends AbstractFOSRestController
             $brand, $price
         );
 
-        //moi : seul les propriétés (pas les méthodes) de la classe Articles suivante seront sérialisées en json
         return $smartPhones;
     }
+
+    /**
+     * @Rest\Get("/phone/{id}", name="show", requirements = {"id"="\d+"})
+     * @Rest\View( 
+     *     statusCode = 200, 
+     *     serializerGroups = {"details"} 
+     * )
+     * 
+     * @OA\Get(
+     *      tags={"Smartphones"},
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          description="Resource id",
+     *          @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Returns phone details",
+     *          @OA\JsonContent(ref=@Model(type=SmartPhone::class, groups={"details"}))
+     *      ),
+     *      @OA\Response(
+     *          response="404",
+     *          description="If the requested smartphone does not exist in our database",
+     *          @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="This phone does not exist in our database")
+     *      )
+     * )
+     * )
+     */
+    public function show(?SmartPhone $smartPhone)
+    {
+        if ($smartPhone) {
+            return $smartPhone;
+        } else {
+            throw new ResourceNotFoundException("This phone does not exist in our database");
+        }
+        
+        
+    }    
 }
